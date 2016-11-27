dae2493b68830d4e1dd7b857d9fe2f3210f9e3b5
Louis Walker <phetablack@gmail.com>
2016-11-27 01:47:12 +0000

    Created dexterity material theme, added logo, added global styles, HTML/CSS for header card, socialProfiles Array<Object> added - note: move to firebase$

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  private socialProfiles: Array<Object> = [];
+
+  constructor() {
+    this.socialProfiles = [
+      {
+        shortName: "facebook",
+        brandName: "Facebook",
+        href: "facebook.com/ngserve"
+      },
+      {
+        shortName: "twitter",
+        brandName: "Twitter",
+        href: "twitter.com/ng_serve"
+      },
+      {
+        shortName: "linkedin",
+        brandName: "LinkedIn",
+        href: "uk.linkedin.com/in/louis-walker-0b334394"
+      },
+      {
+        shortName: "github",
+        brandName: "GitHub",
+        href: "github.com/Louis-Walker"
+      },
+    ];
+  }
 }
